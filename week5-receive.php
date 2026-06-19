9f6a1bd5e6ad2529c9624df3a7cdab7901894662
sakda261050-cloud <sakda261050@gmail.com>
2026-06-19 12:54:06 +0700

    Add files via upload

---
+++ week5-receive.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_GET["uesrname"];
+    $password = $_GET["password"];
+    $email = $_GET["email"];
+    $useraAge = $_GET["useraAge"];
+    $useraBirth = $_GET["useraBirth"];
+    $userGender = $_GET["userGender"];
+    $userProvince = $_GET["userProvince"];
+    $userHobby = $_GET["userHobby"];
+
+    echo "สวัสดี $name<br>";
+    echo "รหัสผ่านของคุณคือ: $password<br>";
+    echo "อีเมลของคุณคือ: $email<br>";
+    echo "อายุของคุณคือ: $useraAge<br>";
+    echo "วันเกิดของคุณคือ: $useraBirth<br>";
+    echo "เพศของคุณคือ: $userGender<br>";
+    echo "จังหวัดของคุณคือ: $userProvince<br>";
+    echo "งานอดิเรกของคุณคือ: " . implode(", ", $userHobby) . "<br>";
+?>
